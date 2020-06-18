420508381415963f8f182cb21d798f3175203fb6
Moromir <moromir24@gmail.com>
2020-06-18 15:04:55 +0200

    no changes

--- src/Bridge/Implementation/Behaviour/eBehaviourType.java
+++ src/Bridge/Implementation/Behaviour/eBehaviourType.java
@@ -10,40 +10,39 @@ package Bridge.Implementation.Behaviour;
 import Bridge.Abstraction.IVirus;
 
 public enum EBehaviourType {
-    //Objets directement construits
-    BEHAVIOUR_LVL_1( BehaviourLvlOne.class),
-    BEHAVIOUR_LVL_2 ( BehaviourLvlTwo.class),
-    BEHAVIOUR_LVL_3( BehaviourLvlThree.class);
+    // Objets directement construits
+    BEHAVIOUR_LVL_1(BehaviourLvlOne.class), BEHAVIOUR_LVL_2(BehaviourLvlTwo.class),
+    BEHAVIOUR_LVL_3(BehaviourLvlThree.class);
 
     private Class _bheaviourType;
 
     public EBehaviourType getNext() {
-        return this.ordinal() < EBehaviourType.values().length - 1
+        return this.ordinal() < EBehaviourType.values().length - 1 
                 ? EBehaviourType.values()[this.ordinal() + 1]
                 : EBehaviourType.values()[0];
     }
 
-    public boolean isLastValue(){
+    public boolean isLastValue() {
         return this.ordinal() == EBehaviourType.values().length - 1;
     }
 
-    //Constructeur
-    EBehaviourType(Class bheaviourType){
+    // Constructeur
+    EBehaviourType(Class bheaviourType) {
         _bheaviourType = bheaviourType;
     }
 
-    public String toString(){
+    public String toString() {
         return _bheaviourType.toString();
     }
 
-    public static EBehaviourType getBehaviourTypeFormInstance(IVirus virus){
+    public static EBehaviourType getBehaviourTypeFormInstance(IVirus virus) {
         String behaviour = virus.getBehaviour().getClass().toString();
-        if(behaviour.equals(BehaviourLvlOne.class.toString())){
+        if (behaviour.equals(BehaviourLvlOne.class.toString())) {
             return BEHAVIOUR_LVL_1;
-        }else if (behaviour.equals(BehaviourLvlTwo.class.toString())){
+        } else if (behaviour.equals(BehaviourLvlTwo.class.toString())) {
             return BEHAVIOUR_LVL_2;
-        }else if (behaviour.equals(BehaviourLvlThree.class.toString())){
-            return  BEHAVIOUR_LVL_3;
+        } else if (behaviour.equals(BehaviourLvlThree.class.toString())) {
+            return BEHAVIOUR_LVL_3;
         }
         return null;
     }
